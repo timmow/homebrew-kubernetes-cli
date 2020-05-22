8a345e4829b87d6090264044ff8c92cc96cc00b2
Tim Mower <timothy.mower@gmail.com>
2020-05-22 14:29:07 +0100

    Iniital Commit

---
+++ Formula/kubernetes-cli@1.16.rb
@@ -0,0 +1,70 @@
+class KubernetesCliAT116 < Formula
+  desc "Kubernetes command-line interface"
+  homepage "https://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      :tag      => "v1.16.3",
+      :revision => "b3cbbae08ec52a7fc73d334838e18d17e8512749"
+  head "https://github.com/kubernetes/kubernetes.git"
+
+  bottle do
+    # force "bottles" subdir for download
+    # https://github.com/glensc/homebrew-tap/pull/1#issuecomment-486371612
+    root_url "https://homebrew.bintray.com/bottles"
+    cellar :any_skip_relocation
+    sha256 "0ffd9ee9bb5026ae526a09fe50591a94a1a987f52c5866e32aefb79b533f6e10" => :catalina
+    sha256 "b8999e5c544b70b0223952fd92eda08eff8035762dae2a27c31aaca24fed51c0" => :mojave
+    sha256 "a97a579a2adaa9720aa122355b0a43cbebea49da394f3489a300f41d8d5e7f77" => :high_sierra
+  end
+
+  depends_on "go@1.12" => :build
+
+  # force "kubernetes-helm" for bottle download
+  # https://github.com/glensc/homebrew-tap/pull/1#issuecomment-486371612
+  def name
+    "kubernetes-cli"
+  end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/k8s.io/kubernetes"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      # Race condition still exists in OS X Yosemite
+      # Filed issue: https://github.com/kubernetes/kubernetes/issues/34635
+      ENV.deparallelize { system "make", "generated_files" }
+
+      # Make binary
+      system "make", "kubectl"
+      bin.install "_output/local/bin/darwin/amd64/kubectl"
+
+      # Install bash completion
+      output = Utils.popen_read("#{bin}/kubectl completion bash")
+      (bash_completion/"kubectl").write output
+
+      # Install zsh completion
+      output = Utils.popen_read("#{bin}/kubectl completion zsh")
+      (zsh_completion/"_kubectl").write output
+
+      prefix.install_metafiles
+
+      # Install man pages
+      # Leave this step for the end as this dirties the git tree
+      system "hack/generate-docs.sh"
+      man1.install Dir["docs/man/man1/*.1"]
+    end
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource)
+                         .instance_variable_get(:@specs)[:revision],
+                   version_output
+    end
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Timmow Kubernetes-cli
+
+## How do I install these formulae?
+`brew install timmow/kubernetes-cli/<formula>`
+
+Or `brew tap timmow/kubernetes-cli` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/timmow/homebrew-kubernetes-cli/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
